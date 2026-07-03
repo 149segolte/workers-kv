e84792469098d66a3c228e407d950212e3f9797a
149segolte <37300847+149segolte@users.noreply.github.com>
2026-07-03 04:39:04 -0400

    feat: add access JWT debug

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,66 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { jwtVerify, createRemoteJWKSet, JWTPayload } from 'jose';
+
+interface Env {
+	POLICY_AUD: string;
+	TEAM_DOMAIN: string;
+}
+
+// Typed responses
+type Payload = any;
+type APIResponse =
+	| {
+			success: true;
+			data: Payload;
+	  }
+	| {
+			success: false;
+			error: string;
+	  };
+
+function createResponse(status: number, body: APIResponse, init?: ResponseInit): Response {
+	return Response.json(body, {
+		...init,
+		status,
+	});
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		// Environment checks
+
+		// Verify environment variables
+		if (!env.POLICY_AUD) {
+			return createResponse(500, { success: false, error: 'Server configuration error: Missing required audience' });
+		}
+		if (!env.TEAM_DOMAIN) {
+			return createResponse(500, { success: false, error: 'Server configuration error: Missing required team domain' });
+		}
+
+		let privileged = false;
+		let token_data: null | JWTPayload = null;
+
+		// Get the JWT from the request headers
+		const token = request.headers.get('cf-access-jwt-assertion');
+		if (token) {
+			try {
+				// Create JWKS from your team domain
+				const JWKS = createRemoteJWKSet(new URL(`${env.TEAM_DOMAIN}/cdn-cgi/access/certs`));
+
+				// Verify the JWT
+				const { payload } = await jwtVerify(token, JWKS, {
+					issuer: env.TEAM_DOMAIN,
+					audience: env.POLICY_AUD,
+				});
+
+				privileged = true;
+				token_data = payload;
+			} catch {
+				// Invalid token
+				return createResponse(401, { success: false, error: 'Invalid token' });
+			}
+		}
+
+		// Return the response
+		return createResponse(200, { success: true, data: { privileged, token_data } });
 	},
 } satisfies ExportedHandler<Env>;
